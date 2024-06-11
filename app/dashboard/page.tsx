7453a685f36f1805b99c4db73d10d423536e0a70
duardo10 <duardos36@gmail.com>
2024-06-10 21:10:55 -0300

    implementando pesquisa e paginação

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { Suspense } from 'react';
+import { lusitana } from '@/app/ui/fonts';
+
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+ 
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      // ...
+    </main>
+  );
 }
